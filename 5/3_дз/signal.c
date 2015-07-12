906fbb17e0c2f96ee26c53c48d676f12e8aa5981
Daniil Vinogradov <xitrix@bk.ru>
2015-07-13 00:24:00 +0300

    test

--- 5/3_дз/signal.c
+++ 5/3_дз/signal.c
@@ -10,33 +10,40 @@
 void handler(int sig, siginfo_t *si, void *uc) {
 	printf("Caught signal %d, sending PID: %ld\n", sig, (long)si->si_pid);
 }
-void handler2(int sig, siginfo_t *si, void *uc) {
-	printf("Caught signal %d, sending PID: %ld\n", sig, (long)si->si_pid);
-}
 
-int main(int argc, char *argv[]) {
-	sigset_t mask;
+void *signalthreed(void *num){
 	struct sigaction sa;
-	struct sigaction sa2;
-
 	printf("Establishing handler for signal %d and %d\n", SIG, SIG2);
 	sa.sa_flags = SA_SIGINFO;
 	sa.sa_sigaction = handler;
 	sigemptyset(&sa.sa_mask);
-	sa2.sa_flags = SA_SIGINFO;
-	sa2.sa_sigaction = handler2;
-	sigemptyset(&sa.sa_mask);
 
 	if (sigaction(SIG, &sa, NULL) == -1) {
 		perror("sigaction");
-		return 1;
+		exit(1);
 	}
 
-	if (sigaction(SIG2, &sa2, NULL) == -1) {
+	if (sigaction(SIG2, &sa, NULL) == -1) {
 		perror("sigaction");
-		return 1;
+		exit(1);
 	}
 
 	while(1);
+}
+
+int main(int argc, char *argv[]) {
+	sigset_t mask;
+	pthread_t thread;
+	int rc;
+
+	rc=pthread_create(&thread,NULL, signalthreed, (void *)0);
+	if(rc){
+		printf("ERROR: %d\n",rc);
+		exit(-1);
+	} 
+
+	pthread_join(thread,NULL);
+
+	
 	return 0;
 }
